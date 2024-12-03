1e70ba459cc0a42b3bd602c3c3a69c103033efa6
Yhano <iantribu_08@hotmail.com>
2024-12-03 13:36:43 +0800

    Update CustomOSDCloud.ps1

--- CustomOSDCloud.ps1
+++ CustomOSDCloud.ps1
@@ -1,4 +1,4 @@
-Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSEdition Pro -OSLanguage en-us -Verbose
+Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSEdition Pro -OSLanguage en-us -Verbose -ErrorAction Stop
 
 # Restart from WinPE
-wpeutil reboot
+wpeutil reboot -ErrorAction Stop
