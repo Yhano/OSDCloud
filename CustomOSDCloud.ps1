0d73e139ffed58222c8f435e86dfead4de0a5845
Yhano <iantribu_08@hotmail.com>
2025-03-15 17:00:02 +0800

    Update CustomOSDCloud.ps1
    Updating

--- CustomOSDCloud.ps1
+++ CustomOSDCloud.ps1
@@ -9,14 +9,24 @@ Install-Module OSD -Force
 Write-Host -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
 Import-Module OSD -Force
 
-# TODO: Spend the time to write a function to do this and put it here
-Write-Host -ForegroundColor Cyan "Ejecting ISO"
-Write-Warning "That didn't work because I haven't coded it yet!"
-# Start-Sleep -Seconds 5
+# Function to eject ISO (Placeholder)
+function Eject-ISO {
+    Write-Host -ForegroundColor Cyan "Ejecting ISO"
+    Write-Warning "That didn't work because I haven't coded it yet!"
+}
+
+Eject-ISO
 
 # Start OSDCloud ZTI the RIGHT way
 Write-Host -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
-Start-OSDCloud -OSName 'Windows 11 23H2 x64' -OSEdition Pro -OSLanguage en-US -OSActivation Volume
+
+# Get the system locale
+$locale = (Get-WinSystemLocale).Name
+
+# Path to the Autopilot offline JSON file
+$autopilotJsonPath = ".\AutopilotConfigurationFile.json"  # Adjust this path if the file is in a different directory
+
+Start-OSDCloud -OSName 'Windows 11 24H2 x64' -OSEdition Pro -OSLanguage $locale -OSActivation Volume -AutopilotFile $autopilotJsonPath
 
 # Anything I want can go right here and I can change it at any time since it is in the Cloud!!!!!
 Write-Host -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
