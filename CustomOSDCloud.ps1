8fbc560d517aadf2a1ee67656143135de0de4316
Yhano <iantribu_08@hotmail.com>
2025-03-19 19:34:22 +0800

    Update CustomOSDCloud.ps1

--- CustomOSDCloud.ps1
+++ CustomOSDCloud.ps1
@@ -61,10 +61,7 @@ if (Test-Path -Path $OOBECmdPath) {
 $OOBECMD = @'
 @echo off
 # Execute OOBE Tasks
-start /wait powershell.exe -WindowStyle Hidden -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\Rename.ps1
-
-# Below a PS session for debug and testing in system context, # when not needed 
-# start /wait powershell.exe -NoL -ExecutionPolicy Bypass
+start /b /wait powershell.exe -WindowStyle Hidden -NoProfile -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\Rename.ps1
 
 exit 
 '@
